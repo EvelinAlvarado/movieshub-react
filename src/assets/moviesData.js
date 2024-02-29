234bd494f0e97ed96acc6fc0c7276ef0f0cf9cfe
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2024-02-29 10:26:51 +0000

    feat: Add movie data list
    Added movie data with properties such as title, description, releaseYear, duration, category, and imageUrl. Includes examples of acclaimed movies in various categories.

---
+++ src/assets/moviesData.js
@@ -0,0 +1,67 @@
+export const dataMovieList = [
+  {
+    id: 1,
+    title: "The Shawshank Redemption",
+    description:
+      "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    releaseYear: "1994",
+    duration: "2h 22min",
+    category: "Drama",
+    posterUrl:
+      "https://m.media-amazon.com/images/M/MV5BMzJkNjNiOWUtYzNmOC00MGFjLWE5N2EtMjdkYjEwMzQ4NzBhXkEyXkFqcGdeQXVyMTAyOTE2ODg0._V1_FMjpg_UX853_.jpg",
+    backgroundImageUrl:
+      "https://m.media-amazon.com/images/M/MV5BMTM0NjUxMDk5MF5BMl5BanBnXkFtZTcwNDMxNDY3Mw@@._V1_FMjpg_UX1800_.jpg",
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    description:
+      "An organized crime dynasty's aging patriarch transfers control of his clandestine empire to his reluctant son.",
+    releaseYear: "1972",
+    duration: "2h 55min",
+    category: "Crime",
+    posterUrl:
+      "https://m.media-amazon.com/images/M/MV5BYjU2M2YzYTMtYzk4NS00NzFlLTg3ZGQtOThlZjdmOGFkNGEyXkEyXkFqcGdeQXVyMTU1NzIyMDE0._V1_FMjpg_UY3000_.jpg",
+    backgroundImageUrl:
+      "https://m.media-amazon.com/images/M/MV5BMjA0MDIwMDYwNl5BMl5BanBnXkFtZTcwMjY0Mzg4Mw@@._V1_FMjpg_UX1280_.jpg",
+  },
+  {
+    id: 3,
+    title: "Schindler's List",
+    description:
+      "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
+    releaseYear: "1993",
+    duration: "3h 15min",
+    category: "Biography",
+    posterUrl:
+      "https://m.media-amazon.com/images/M/MV5BOWRmZmVmMGItODgwNS00Y2VjLWEyZjktOWRjNDVhMzkyY2UwXkEyXkFqcGdeQXVyMTAyOTE2ODg0._V1_FMjpg_UX960_.jpg",
+    backgroundImageUrl:
+      "https://m.media-amazon.com/images/M/MV5BMTgwNzg3MDkxM15BMl5BanBnXkFtZTcwMzA2MDAxMw@@._V1_FMjpg_UX1280_.jpg",
+  },
+  {
+    id: 4,
+    title: "The Dark Knight",
+    description:
+      "When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    releaseYear: "2008",
+    duration: "2h 32min",
+    category: "Action",
+    posterUrl:
+      "https://m.media-amazon.com/images/M/MV5BMTU0NTEzMTIzM15BMl5BanBnXkFtZTcwMDQxMzU1MQ@@._V1_FMjpg_UX550_.jpg",
+    backgroundImageUrl:
+      "https://m.media-amazon.com/images/M/MV5BNjExODU3ZjEtMzBjMy00YjY3LThlZGUtZGFjNmZjMTZlZmI3XkEyXkFqcGdeQXVyMjQwMDg0Ng@@._V1_FMjpg_UX1280_.jpg",
+  },
+  {
+    id: 5,
+    title: "Forrest Gump",
+    description:
+      "The presidencies of Kennedy and Johnson, the Vietnam War, the Watergate scandal, and other historical events unfold from the perspective of an Alabama man with an IQ of 75.",
+    releaseYear: "1994",
+    duration: "2h 22min",
+    category: "Drama",
+    posterUrl:
+      "https://m.media-amazon.com/images/M/MV5BOWNmNzdjYjQtZjIyOS00MDhiLTg4ZWUtZGZkZDE4ZDQwZTY1XkEyXkFqcGdeQXVyODk2ODI3MTU@._V1_FMjpg_UX960_.jpg",
+    backgroundImageUrl:
+      "https://m.media-amazon.com/images/M/MV5BMTZkY2NiZmUtMDViYS00ZWNmLTk5MzItZThjZGFmZWZmODU1XkEyXkFqcGdeQXVyNjI5Nzc0MTY@._V1_FMjpg_UX720_.jpg",
+  },
+];
