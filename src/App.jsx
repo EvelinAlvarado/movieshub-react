01566d728f70f34416c8537ee5220f384473fe9f
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2024-03-08 16:47:09 +0000

    Install react-router-dom and modify App function
    Installed the 'react-router-dom' package to enable routing in the application. Updated the App function to use BrowserRouter, Routes, and Route components for navigation. Added new routes for the SyncedSliders, FormNewMovie, and FormNewCategory components. Integrated Header and Footer components for a complete layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header/Header.jsx";
 import { Footer } from "./components/Footer/Footer.jsx";
 import { SyncedSliders } from "./components/SyncedSliders/SyncedSliders.jsx";
@@ -8,13 +9,18 @@ import { FormNewCategory } from "./components/FormNewCategory/FormNewCategory.js
 function App() {
   return (
     <>
-      <Header />
-      <main>
-        {/* <SyncedSliders /> */}
-        {/* <FormNewMovie /> */}
-        <FormNewCategory />
-      </main>
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<SyncedSliders />} />
+            <Route path="/form-new-movie" element={<FormNewMovie />} />
+            <Route path="/form-new-category" element={<FormNewCategory />} />
+            {/* Add Page404 */}
+          </Routes>
+        </main>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
