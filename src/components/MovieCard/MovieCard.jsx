b4bdc52ca7e2e3765385c8699449d90174e7267e
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2024-04-10 19:04:26 +0100

    Implemented MovieCard component with styled components

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,11 +1,25 @@
 import PropTypes from "prop-types";
-import "./MovieCard.css";
+import styled from "styled-components";
+
+const MovieCardDiv = styled.div`
+  padding-left: 1rem;
+  img {
+    max-width: 90%;
+    cursor: pointer;
+    box-shadow: var(--shadow);
+    border-radius: 7px;
+    img:hover {
+      box-sizing: border-box;
+      border: 2px solid var(--primary-color);
+    }
+  }
+`;
 
 export const MovieCard = ({ posterUrl, title }) => {
   return (
-    <div className="movie-card">
+    <MovieCardDiv>
       <img src={posterUrl} alt={title} />
-    </div>
+    </MovieCardDiv>
   );
 };
 
